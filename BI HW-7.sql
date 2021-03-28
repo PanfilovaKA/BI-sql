9c7f87e9e3f5d433babb8a35f514152c7be55fce
Ksenia <kseniapanfilova@mail.ru>
2021-03-28 20:57:03 +0300

    Lesson 7

---
+++ BI HW-7.sql
@@ -0,0 +1,103 @@
+DROP TABLE IF EXISTS orders;
+
+CREATE TABLE orders (
+  id SERIAL PRIMARY KEY,
+  user_id INT UNSIGNED,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  KEY index_of_user_id(user_id)
+);
+
+DROP TABLE IF EXISTS orders_products;
+
+CREATE TABLE orders_products (
+  id SERIAL PRIMARY KEY,
+  order_id INT UNSIGNED,
+  product_id INT UNSIGNED,
+  total INT UNSIGNED DEFAULT 1,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+DROP TABLE IF EXISTS users;
+ CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255),
+  birthday_at DATE,
+  created_at VARCHAR(255),
+  updated_at VARCHAR(255)
+);
+
+ INSERT INTO
+  users (name, birthday_at, created_at, updated_at)
+ VALUES
+  ('Геннадий', '1990-10-05', '07.01.2016 12:05', '07.01.2016 12:05'),
+  ('Наталья', '1984-11-12', '20.05.2016 16:32', '20.05.2016 16:32'),
+  ('Александр', '1985-05-20', '14.08.2016 20:10', '14.08.2016 20:10'),
+  ('Сергей', '1988-02-14', '21.10.2016 9:14', '21.10.2016 9:14'),
+  ('Иван', '1998-01-12', '15.12.2016 12:45', '15.12.2016 12:45'),
+  ('Мария', '2006-08-29', '12.01.2017 8:56', '12.01.2017 8:56');
+ 
+INSERT INTO orders (user_id)
+SELECT id FROM users WHERE name = 'Геннадий';
+
+DROP TABLE IF EXISTS products;
+CREATE TABLE products (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) COMMENT 'Название',
+  desription TEXT COMMENT 'Описание',
+  price DECIMAL (11,2) COMMENT 'Цена',
+  catalog_id INT UNSIGNED,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  KEY index_of_catalog_id (catalog_id)
+);
+
+INSERT INTO products
+  (name, description, price, catalog_id)
+VALUES
+  ('Intel Core i3-8100', 'Процессор для настольных персональных компьютеров, основанных на платформе Intel.', 7890.00, 1),
+  ('Intel Core i5-7400', 'Процессор для настольных персональных компьютеров, основанных на платформе Intel.', 12700.00, 1),
+  ('AMD FX-8320E', 'Процессор для настольных персональных компьютеров, основанных на платформе AMD.', 4780.00, 1),
+  ('AMD FX-8320', 'Процессор для настольных персональных компьютеров, основанных на платформе AMD.', 7120.00, 1),
+  ('ASUS ROG MAXIMUS X HERO', 'Материнская плата ASUS ROG MAXIMUS X HERO, Z370, Socket 1151-V2, DDR4, ATX', 19310.00, 2),
+  ('Gigabyte H310M S2H', 'Материнская плата Gigabyte H310M S2H, H310, Socket 1151-V2, DDR4, mATX', 4790.00, 2),
+  ('MSI B250M GAMING PRO', 'Материнская плата MSI B250M GAMING PRO, B250, Socket 1151, DDR4, mATX', 5060.00, 2);
+
+INSERT INTO orders_products (order_id, product_id, total)
+SELECT LAST_INSERT_ID(), id, 2 FROM products
+WHERE name = 'Intel Core i5-7400';
+
+INSERT INTO orders (user_id)
+SELECT id FROM users WHERE name = 'Наталья';
+
+INSERT INTO orders_products (order_id, product_id, total)
+SELECT LAST_INSERT_ID(), id, 1 FROM products
+WHERE name IN ('Intel Core i5-7400', 'Gigabyte H310M S2H');
+
+SELECT id, name, birthday_at FROM users;
+
+SELECT u.id, u.name, u.birthday_at
+  FROM users AS u
+   JOIN orders AS o
+     ON u.id = o.user_id;
+CREATE TABLE catalogs (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255),
+  UNIQUE unique_name(name(10))
+);
+
+INSERT INTO catalogs VALUES
+  (NULL, 'Процессоры'),
+  (NULL, 'Материнские платы'),
+  (NULL, 'Видеокарты'),
+  (NULL, 'Жесткие диски'),
+  (NULL, 'Оперативная память');     
+
+ SELECT
+  p.id,
+  p.name,
+  p.price,
+  c.name AS catalog
+  FROM products AS p
+    LEFT JOIN catalogs AS c
+      ON p.catalog_id = c.id;
